c4b66715344e064a99babd3f0e60f2cddfa2f289
Ritesh Yadav <yadavri@thoughtworks.com>
2020-12-12 23:19:14 +0000

    update install recipe script

--- assets/scripts/install_recipe_reader.sh
+++ assets/scripts/install_recipe_reader.sh
@@ -4,16 +4,18 @@ command_exists() {
 	command -v "$@" >/dev/null 2>&1
 }
 
-command_exists curls && {
-    curl -fsSL https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/assets/scripts/recipe_reader.sh -O
-    curl -fsSL "https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/_includes/recipes/$1" -O
-    bash recipe_reader.sh "$1"
+rm -f recipe_reader.sh "$0"
+
+command_exists curl && {
+    curl -fsSL https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/assets/scripts/recipe_reader.sh -o recipe_reader.sh
+    curl -fsSL "https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/_includes/recipes/$0" -o "$0"
+    bash recipe_reader.sh "$0"
     exit 0
 }
 
 command_exists wget && {
-    wget -O- https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/assets/scripts/recipe_reader.sh
-    wget -O- "https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/_includes/recipes/$1"
-    bash recipe_reader.sh "$1"
+    wget https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/assets/scripts/recipe_reader.sh -o recipe_reader.sh
+    wget "https://raw.githubusercontent.com/Ritesh-Yadav/Ritesh-Yadav.github.io/master/_includes/recipes/$0" -o "$0"
+    bash recipe_reader.sh "$0"
     exit 0
 }
